cbdc80f53b13b939e2cdf83eec341efb5f7d4337
ISHADYI <ShadyGame1@mail.ru>
2023-10-03 14:17:11 +0300

    init project

---
+++ main.js
@@ -0,0 +1,25 @@
+import 'normalize.css'
+import './style.scss'
+
+//==================SWIPER==================
+import { Navigation, Thumbs } from 'swiper/modules';
+import Swiper from 'swiper';
+import 'swiper/css';
+const swiperThumbnails = new Swiper(".product__slider-thumbnails", {
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+new Swiper(".product__swiper-main", {
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".product__arrow-next",
+    prevEl: ".product__arrow-prev",
+  },
+  modules: [Navigation, Thumbs],
+  thumbs: {
+    swiper: swiperThumbnails,
+  },
+});
+//===========================================
