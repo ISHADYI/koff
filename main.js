e608ec8eb94b0d236b03a0d4f72a44ced6088a7d
ISHADYI <ShadyGame1@mail.ru>
2023-10-10 23:36:15 +0300

    add js

--- main.js
+++ main.js
@@ -1,25 +1,73 @@
 import 'normalize.css'
 import './style.scss'
-
+import Navigo from 'navigo';
+import { Header } from './modules/Header/Header';
+import { Main } from './modules/Main/Main';
+import { Footer } from './modules/Footer/Footer';
 //==================SWIPER==================
-import { Navigation, Thumbs } from 'swiper/modules';
-import Swiper from 'swiper';
-import 'swiper/css';
-const swiperThumbnails = new Swiper(".product__slider-thumbnails", {
-  spaceBetween: 10,
-  slidesPerView: 4,
-  freeMode: true,
-  watchSlidesProgress: true,
-});
-new Swiper(".product__swiper-main", {
-  spaceBetween: 10,
-  navigation: {
-    nextEl: ".product__arrow-next",
-    prevEl: ".product__arrow-prev",
-  },
-  modules: [Navigation, Thumbs],
-  thumbs: {
-    swiper: swiperThumbnails,
-  },
-});
+const productSlider = () => {
+  Promise.all([
+    import('swiper/modules'),
+    import('swiper'),
+    import('swiper/css')
+  ]).then(([{Navigation, Thumbs}, Swiper]) => {
+    const swiperThumbnails = new Swiper.default(".product__slider-thumbnails", {
+      spaceBetween: 10,
+      slidesPerView: 4,
+      freeMode: true,
+      watchSlidesProgress: true,
+    });
+    new Swiper.default(".product__swiper-main", {
+      spaceBetween: 10,
+      navigation: {
+        nextEl: ".product__arrow_next",
+        prevEl: ".product__arrow_prev",
+      },
+      modules: [Navigation, Thumbs],
+      thumbs: {
+        swiper: swiperThumbnails,
+      },
+    });
+  });
+};
 //===========================================
+const init = () => {
+  new Header().mount();
+  new Main().mount();
+  new Footer().mount();
+
+  productSlider();
+
+  const router = new Navigo("/", { linksSelector: 'a[href^="/"]' });
+
+  router
+    .on("/", () => {
+      console.log('На главной');
+    })
+    .on("/favorite", () => {
+      console.log('favorite');
+    })
+    .on("/cart", () => {
+      console.log('cart');
+    })
+    .on("/search", () => {
+      console.log('search');
+    })
+    .on("/category", (obj) => {
+      console.log(obj);
+      console.log('category');
+    })
+    .on("/product/:id", (obj) => {
+      console.log(obj);
+    })
+    .on("order", () => {
+          new Order().mount(new Main().element);
+          console.log("Заказ");
+    })
+    .notFound(() => {
+      console.log(404);
+    })
+
+  router.resolve();
+};
+init();
---
+++ modules/Order/Order.js
@@ -0,0 +1,97 @@
+import { addContainer } from "../addContainer";
+
+export class Order {
+	static instance = null;
+	
+	constructor() {
+		if(!Order.instance) {
+			Order.instance = this;
+			this.element = document.createElement('section');
+			this.element.classList.add("order");
+			this.element.setAttribute("name", "order")
+			this.containerElement = addContainer(this.element, "order__container");
+			this. isMounted = false;
+		}
+		return Order.instance;
+	}
+
+	mount(parent) {
+		if (this.isMounted) {
+			return;
+		}
+
+		const orderHeader = document.createElement("div")
+		orderHeader.classList.add("order__header")
+
+		const orderHeaderTitle = document.createElement("h3")
+		orderHeaderTitle.classList.add("order__title")
+		orderHeaderTitle.textContent("Заказ успешно размещен")
+
+		const orderPrice = document.createElement("p");
+		orderPrice.classList.add("order__price")
+		orderPrice.textContent = `20&nbsp;000&nbsp;₽`;
+
+		orderHeader.append(orderHeaderTitle, orderPrice)
+
+		const orderNumber = document.createElement("p")
+		orderNumber.classList.add("order__number")
+		orderNumber.textContent = `№${Number(43435)}`
+
+		this.containerElement.append(orderHeader, orderNumber, getOrderData());
+
+		parent.append(this.element);
+		this.isMounted = true;
+	}
+
+	unmount() {
+		this.element.remove();
+		this.isMounted = false;
+	}
+
+	getOrderData() {
+		const orderData = document.createElement("div");
+		orderData.classList.add("order__data")
+
+		const orderTitle = document.createElement("h4");
+		orderTitle.classList.add("order__data-title");
+		orderTitle.textContent("Данные доставки");
+
+		const orderBtn = document.createElement("button");
+		orderBtn.classList.add("order__button", "button");
+		orderBtn.textContent = "На главную";
+		orderBtn.type = "button"
+
+		orderData.append(orderTitle, getOrderTable(), orderBtn)
+	}
+
+	getOrderTable() {
+		return `
+		<table class="order__data-table table">
+		<tr class="table__row">
+			<td class="table__field">Получатель</td>
+			<td class="table__value">Иванов Петр Александрович</td>
+		</tr>
+		<tr class="table__row">
+			<td class="table__field">Телефон</td>
+			<td class="table__value">+7 (737) 346 23 00</td>
+		</tr>
+		<tr class="table__row">
+			<td class="table__field">E-mail</td>
+			<td class="table__value">Ivanov84@gmail.com</td>
+		</tr>
+		<tr class="table__row">
+			<td class="table__field">Адрес доставки</td>
+			<td class="table__value">Москва, ул. Ленина, 21, кв. 33</td>
+		</tr>
+		<tr class="table__row">
+			<td class="table__field">Способ оплаты</td>
+			<td class="table__value">Картой при получении</td>
+		</tr>
+		<tr class="table__row">
+			<td class="table__field">Способ получения</td>
+			<td class="table__value">Доставка</td>
+		</tr>
+	</table>
+		`;
+	}
+}
